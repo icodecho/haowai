0433ca61b92a9c7a628eb8e613d4e8de574cdaab
icodecho <9eeks.lover@gmail.com>
2026-04-17 04:19:04 +0800

    fixed

--- app/src/main/java/evilcode/notification/hwpush/MainActivity.java
+++ app/src/main/java/evilcode/notification/hwpush/MainActivity.java
@@ -65,7 +65,6 @@ public class MainActivity extends AppCompatActivity {
                 if (log != null) {
                     appendLog(log);
                 }
-                refreshMessageList();
             }
         }
     };
